d4e09338b4e4ed679428cf2f10a0cf74f400b194
Cammy Smith <cjsarab1991@gmail.com>
2022-10-31 21:47:05 +0000

    yahtzee

--- yahtzee/client/src/components/scorecard/lower/Yahtzee.js
+++ yahtzee/client/src/components/scorecard/lower/Yahtzee.js
@@ -1,8 +1,51 @@
 import React from 'react';
 
-const Yahtzee = () => {
+const Yahtzee = ({
+  currentRoll,
+  score,
+
+  pushScore,
+  addNumbers,
+  endTurn
+  }) => {
+
+  const calculateScore = (currentRoll) => {
+
+    const thisScore1 = currentRoll.filter(number => number === 1);
+    const thisScore2 = currentRoll.filter(number => number === 2);
+    const thisScore3 = currentRoll.filter(number => number === 3);
+    const thisScore4 = currentRoll.filter(number => number === 4);
+    const thisScore5 = currentRoll.filter(number => number === 5);
+    const thisScore6 = currentRoll.filter(number => number === 6);
+
+    if (thisScore1.length == 5 || 
+        thisScore2.length == 5 ||
+        thisScore3.length == 5 ||
+        thisScore4.length == 5 ||
+        thisScore5.length == 5 ||
+        thisScore6.length == 5 ) 
+        {
+    score.splice(14, 1, 50);
+
+    pushScore(score);
+    endTurn();
+    }
+    else {
+      score.splice(14, 1, 0)
+      pushScore(score)
+      endTurn();
+    };
+  };
+
+  const handleClick = function() {
+    calculateScore(currentRoll);
+  };
+
   return (
-    <div>Yahtzee</div>
+    <>
+    <button onClick={handleClick}>Yahtzee</button>
+    <div> {score[14]} </div>
+    </>
   );
 };
 
