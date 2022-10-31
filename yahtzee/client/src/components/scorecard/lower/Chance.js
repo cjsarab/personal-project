45d14a3962c505372ea7571c8d0dd67bf4d11634
Cammy Smith <cjsarab1991@gmail.com>
2022-10-31 21:43:28 +0000

    Chance

--- yahtzee/client/src/components/scorecard/lower/Chance.js
+++ yahtzee/client/src/components/scorecard/lower/Chance.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
-const Chance = () => {
+const Chance = ({
+  currentRoll,
+  score,
+
+  pushScore,
+  addNumbers,
+  endTurn
+  }) => {
+
+  const calculateScore = (currentRoll) => {
+
+    currentRoll = currentRoll.reduce(addNumbers);
+    score.splice(13, 1, currentRoll);
+
+    pushScore(score);
+    endTurn();
+    };
+
+  const handleClick = function() {
+    calculateScore(currentRoll);
+  };
+
   return (
-    <div>Chance</div>
+    <>
+    <button onClick={handleClick}>Chance</button>
+    <div> {score[13]} </div>
+    </>
   );
 };
 
